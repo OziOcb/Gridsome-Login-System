f6bef7d9ac1a1189313a10dd53c314e227f3936e
OziOcb <pawel_ozi@o2.pl>
2020-06-10 15:38:19 +0100

    Create basic FormLogIn component

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -2,12 +2,19 @@
   <Layout>
     <div class="signIn">
       <h1>This is a SignIn page</h1>
-    </div></Layout
-  >
+
+      <FormLogIn />
+    </div>
+  </Layout>
 </template>
 
 <script>
+import FormLogIn from "@/components/FormLogIn.vue";
+
 export default {
   name: "SignIn",
+  components: {
+    FormLogIn,
+  },
 };
 </script>

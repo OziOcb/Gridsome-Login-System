b23d77ef2d998fe9aa0d141e82475f941668f5f2
OziOcb <pawel_ozi@o2.pl>
2020-06-10 15:56:36 +0100

    Create a working form inside the FormLogIn component

--- src/components/FormLogIn.vue
+++ src/components/FormLogIn.vue
@@ -1,9 +1,56 @@
 <template>
-  <div>FormLogIn</div>
+  <form class="form" @submit.prevent="submit">
+    <div class="form__item formItem">
+      <label for="username" class="formItem__label">Username</label>
+      <input
+        class="formItem__input"
+        v-model="form.username"
+        name="username"
+        id="username"
+        type="text"
+        placeholder="username"
+      />
+    </div>
+
+    <div class="form__item formItem">
+      <label for="email" class="formItem__label">Email</label>
+      <input
+        class="formItem__input"
+        v-model="form.password"
+        name="email"
+        id="email"
+        type="password"
+        placeholder="email"
+      />
+    </div>
+
+    <div class="form__item">
+      <button class="form__btn" :disabled="isPending">Login</button>
+    </div>
+  </form>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "FormLogIn",
+  data() {
+    return {
+      isPending: false,
+      form: {
+        username: "paul",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    submit() {
+      this.isPending = true;
+      console.log("form submit!");
+
+      // TODO: Send the form data to fetch the token (vuex)
+    },
+  },
+};
 </script>
 
 <style></style>
